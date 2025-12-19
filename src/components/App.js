762b74a14db2c9741f3f5b7c25735e71fcc75854
= <=>
2025-12-19 14:42:30 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [gender, setGender] = useState("");
+
+  const products = {
+    Male: [
+      "Shirts",
+      "Facial-grooming kits",
+      "Hair styling",
+      "Jeans",
+      "Shoes"
+    ],
+    Female: [
+      "Tops",
+      "Skirts",
+      "Makeup items",
+      "Jewellery",
+      "Sandals"
+    ]
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <select onChange={(e) => setGender(e.target.value)}>
+        <option value="">Select Gender</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
+
+      <ul>
+        {gender &&
+          products[gender].map((item, index) => (
+            <li key={`${gender}-${index}`}>{item}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
